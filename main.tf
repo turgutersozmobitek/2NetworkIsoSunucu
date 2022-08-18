bd762b762f621420e0378c7c3dd905269e203480
turgutersozmobitek <98022847+turgutersozmobitek@users.noreply.github.com>
2022-08-19 01:25:03 +0300

    Add files via upload

---
+++ main.tf
@@ -0,0 +1,80 @@
+#bir oluşturalım 
+
+resource "vcd_vapp_org_network" "vappOrgNet" {
+  org = var.org_name
+  vdc = var.vdc_name
+  vapp_name = var.vapp_name
+
+  # Comment below line to create an isolated vApp network
+  org_network_name = var.org_network_name
+ 
+}
+
+resource "vcd_vapp_org_network" "vappOrgNet2" {
+  org = var.org_name
+  vdc = var.vdc_name
+  vapp_name = var.vapp_name
+
+  # Comment below line to create an isolated vApp network
+  org_network_name = var.org2_network_name
+ 
+}
+
+resource "vcd_independent_disk" "myNewIndependentDisk" {
+  org             = var.org_name
+  vdc             = var.vdc_name
+  name            = var.vm_disk_name
+  size_in_mb      = var.vm_disk_size
+  bus_sub_type    = "SCSI"
+}
+
+
+resource "vcd_vapp_vm" "web1" {
+  vapp_name     = var.vapp_name
+  name          = var.vm_name
+  computer_name = var.computer_name
+  catalog_name  = var.catalog_name
+  template_name = var.template_name
+  memory        = var.vm_memory
+  cpus          = var.vm_cpu
+  cpu_cores     = var.vm_cpu_core
+  cpu_hot_add_enabled=true
+  memory_hot_add_enabled=true
+  power_on=false
+
+
+  disk {
+    name        = var.vm_disk_name
+    vapp_name   = var.vapp_name
+    vm_name     = var.vm_name
+    bus_number  = 0
+    unit_number = 0
+  }
+
+
+
+  network {
+    type               = "org"
+    name               = var.org_network_name
+    ip_allocation_mode = "MANUAL"
+    ip                 = var.vm_ip
+    adapter_type       = var.adapter_type
+    is_primary         = true
+  }
+
+  network {
+    type               = "org"
+    name               = var.org2_network_name
+    ip_allocation_mode = "MANUAL"
+    ip                 = var.vmorg2_ip
+    adapter_type       = var.2adapter_type
+    is_primary         = false
+  }
+
+
+   customization{
+      enabled=false
+    }
+
+
+}
